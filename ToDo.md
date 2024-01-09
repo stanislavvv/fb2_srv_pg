ca17e2b51b8a310ff550a2a1e4667b821992d334
Stanislav Vlasov <11715503+stanislavvv@users.noreply.github.com>
2024-01-09 17:09:19 +0500

    gunicorn from venv

--- ToDo.md
+++ ToDo.md
@@ -1,6 +1,5 @@
 # ToDo
 
-- add requirements.txt and virtualenv create and use in scripts [higher priority as python3.7 deprecation]
 - use newer flask
 - use HIDE_DELETED config option (forgot to implement)
 - languages to std view (for example: 'RU|RU-ru|ru-' --> 'ru')
--- gunicorn.sh
+++ gunicorn.sh
@@ -5,4 +5,4 @@ if [[ -d venv ]]; then
 fi
 
 BIND="0.0.0.0:8000"
-gunicorn3 -e FLASK_ENV=prod --bind="${BIND}" --timeout=600 --workers=3 'app:create_app()' --access-logfile -
+gunicorn -e FLASK_ENV=prod --bind="${BIND}" --timeout=600 --workers=3 'app:create_app()' --access-logfile -
